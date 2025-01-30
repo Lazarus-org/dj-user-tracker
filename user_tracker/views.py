57946390267170c026c574a3a9bf3311543ec741
ARYAN-NIKNEZHAD <aryan513966@gmail.com>
2025-01-31 01:07:34 +0330

    feat(views): Add base view for active-users report

--- user_tracker/views.py
+++ user_tracker/views.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets, mixins
 from rest_framework.permissions import IsAdminUser
 from django.utils.timezone import now
+from django.shortcuts import render
+
 from datetime import timedelta
 from .models import ActiveUser
 from .serializers import ActiveUserSerializer
@@ -12,3 +14,13 @@ class ActiveUserViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.R
     def get_queryset(self):
         threshold = now() - timedelta(minutes=10)  # Consider users active within the last 10 minutes
         return ActiveUser.objects.filter(last_activity__gte=threshold)
+
+
+
+def active_users_view(request):
+    threshold = now() - timedelta(minutes=10)
+    active_users = ActiveUser.objects.select_related("user").filter(last_activity__gte=threshold).order_by('-last_activity')
+    context = {
+        'active_users': active_users
+    }
+    return render(request, 'active_users.html', context)
